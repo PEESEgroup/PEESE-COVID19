b23aedb50e1835d546fa7f3cce8351db6add3a92
Akshay <ajagekar.akshay@gmail.com>
2020-03-30 17:22:49 -0400

    Updated data_update

--- data_update.py
+++ data_update.py
@@ -1,7 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 import time
+import csv
+import datetime
+import math
 
+# Scrape date
 def get_page():
     url = "https://coronavirus.health.ny.gov/county-county-breakdown-positive-cases"
     page = requests.get(url).text
@@ -18,11 +22,96 @@ time.sleep(5)
 #     count += 0
 
 soup = BeautifulSoup(page, 'lxml')
+
+rd = soup.findAll('div', attrs={'class':'wysiwyg--field-webny-wysiwyg-title'})
 tb = soup.find('table')
 
-row_src = []
+
+recent_date = rd[0].get_text()
+date_obj = datetime.datetime.strptime(recent_date, 'Last update: %B %d, %Y | %I:%M%p')
+date_header = str(date_obj.month) + "/" + str(date_obj.day) + "/" + str(date_obj.year)
+
+data = []
 td = tb.find_all('td')
 for i in range(len(td)//2):
     #print(td[2*i].get_text())
-    row_src.append([td[2*i].get_text(), int(td[2*i+1].get_text().replace(",",""))])
+    data.append([td[2*i].get_text(), int(td[2*i+1].get_text().replace(",",""))])
+
+data.pop(-1)
+
+row_src = [["county", date_header]]
+row_src.extend(data)
+
+
+# Write data
+target = "C:\\Users\\Akshay\\Documents\\GitHub\\PEESE-COVID19\\ny cases by county - ls.csv"
+target2 = "C:\\Users\\Akshay\\Documents\\GitHub\\PEESE-COVID19\\ny cases by county.csv"
+
+row_tgt = []
+with open(target, 'r') as csvfile:
+    csvreader = csv.reader(csvfile)
+    for row in csvreader:
+        row_tgt.append(row)
+
+
+row_tgt2 = []
+with open(target2, 'r') as csvfile:
+    csvreader = csv.reader(csvfile)
+    for row in csvreader:
+        row_tgt2.append(row)
+
+
+headt = row_tgt.pop(0)
+heads = row_src.pop(0)
+for e in heads[1:]:
+    headt.extend([e, e+"/ls"])
+
+headt2 = row_tgt2.pop(0)
+for e in heads[1:]:
+    headt2.extend([e])
+
+
+# adjust 0 values
+err = 1E-2
+
+for rt in row_tgt:
+    is_present = False
+    for rs in row_src:
+        if rt[1].lower() == rs[0].lower():
+            for e in rs[1:]:
+                try:
+                    rt.extend([e, math.log10(int(e)+err)])
+                except ValueError:
+                    rt.extend([0, math.log10(0+err)])
+            is_present = True
+            break
+    if not is_present:
+        rt.extend([0, math.log10(0+err)] * len(rs[1:]))
+
+for rt in row_tgt2:
+    is_present = False
+    for rs in row_src:
+        if rt[0].lower() == rs[0].lower() or rt[0].lower() + " city" == rs[0].lower():
+            for e in rs[1:]:
+                rt.extend([e])
+            is_present = True
+            break
+    if not is_present:
+        rt.extend([0] * len(rs[1:]))
+
+data = [headt]
+data.extend(row_tgt)
+
+data2 = [headt2]
+data2.extend(row_tgt2)
+
+with open("C:\\Users\\Akshay\\Documents\\GitHub\\PEESE-COVID19\\test.csv", 'w', newline="") as wfile:
+    writer = csv.writer(wfile)
+    for r in data:
+        writer.writerow(r)
+
+with open("C:\\Users\\Akshay\\Documents\\GitHub\\PEESE-COVID19\\test2.csv", 'w', newline="") as wfile:
+    writer = csv.writer(wfile)
+    for r in data2:
+        writer.writerow(r)
 
